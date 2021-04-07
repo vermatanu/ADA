c54325cbe8c52693b0cf5dd9c66f00c427c91c6b
vermatanu <68367808+vermatanu@users.noreply.github.com>
2021-04-07 10:32:54 +0530

    Add files via upload

---
+++ ADA/Knapsack_dynamic.java
@@ -0,0 +1,31 @@
+public class Knapsack_dynamic {
+    static int knapSack(int W, int wt[], int val[], int n) {
+
+        int i, w;
+
+        int[][] K = new int[n + 1][W + 1];
+
+        for (i = 0; i <= n; i++) {
+
+            for (w = 0; w <= W; w++) {
+
+                if (i == 0 || W == 0)
+
+                    K[i][w] = 0;
+
+                else if (wt[i - 1] <= w)
+
+                    K[i][w] = Math.max(val[i - 1] + K[i - 1][w - wt[i - 1]], K[i - 1][w]);
+                else
+
+                    K[i][w] = K[i - 1][w];
+            }
+        }
+        return K[n][W];
+    }
+    public static void main(String[] args) {
+        int wt[]={1,2,3,4,5,6};
+        int val[]={6,5,4,3,2,1};
+        System.out.println("the max output is :"+ knapSack(3, wt, val, 3));
+    }
+}
